a2b9f6f2fd01c664241e873d07ed9b15cddfd62c
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-16 03:12:28 +0400

    developed

--- src/main/java/com/example/librarymanagementsystem/controller/BookContoller.java
+++ src/main/java/com/example/librarymanagementsystem/controller/BookContoller.java
@@ -3,6 +3,7 @@ package com.example.librarymanagementsystem.controller;
 import com.example.librarymanagementsystem.entity.Book;
 import com.example.librarymanagementsystem.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,5 +35,15 @@ public class BookContoller {
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteById(id);
     }
+    @PostMapping("/{bookId}/borrow/{userId}")
+    public ResponseEntity<Book> borrowBook(@PathVariable Long bookId,@PathVariable Long userId) {
+        Book borrowedBook = bookService.borrowBook(bookId,userId);
+        if(borrowedBook!=null) {
+            return ResponseEntity.ok(borrowedBook);
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 
 }
