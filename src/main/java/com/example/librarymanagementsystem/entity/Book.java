57c8c33113871de0835d23d85ddb1ed0519772d7
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-16 02:27:28 +0400

    deveolped

---
+++ src/main/java/com/example/librarymanagementsystem/entity/Book.java
@@ -0,0 +1,22 @@
+package com.example.librarymanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private boolean borrowed;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User borrowedBy;
+
+
+}
