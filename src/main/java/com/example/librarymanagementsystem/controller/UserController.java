6a76907fa41f852b89b0be2707819cb0b5b6fcfa
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-16 02:55:06 +0400

    developed

---
+++ src/main/java/com/example/librarymanagementsystem/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.librarymanagementsystem.controller;
+
+import com.example.librarymanagementsystem.entity.User;
+import com.example.librarymanagementsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User addUser() {
+
+    }
+}
