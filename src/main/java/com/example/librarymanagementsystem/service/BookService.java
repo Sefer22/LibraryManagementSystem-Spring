4c802ec57e1e1a0f469f5f33ebcf5f36bacef9cd
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-16 02:36:14 +0400

    developed

--- src/main/java/com/example/librarymanagementsystem/service/BookService.java
+++ src/main/java/com/example/librarymanagementsystem/service/BookService.java
@@ -1,7 +1,26 @@
 package com.example.librarymanagementsystem.service;
 
+import com.example.librarymanagementsystem.entity.Book;
+import com.example.librarymanagementsystem.repository.BookRepository;
+import com.example.librarymanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+    public Book findById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
 }
